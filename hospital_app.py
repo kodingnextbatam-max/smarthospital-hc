c3ccdb4999e3c7e05c1bc35df55a227ae1dd1cb7
kodingnextbatam-max <kodingnextbatam@gmail.com>
2026-07-01 15:21:10 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -109,7 +109,7 @@ if st.button("Predict Department"):
 
 patinet_scaled = patient.copy()
 
-patient_scaled[cols_to_scale = scaler.transform(
+patient_scaled[cols_to_scale] = scaler.transform(
   patient[cols_to_scale]
 )
 
